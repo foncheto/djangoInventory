d2da3f06b92dfa9a88e40ab7ab7241c4cc9ed541
Alfonso <81327113+foncheto@users.noreply.github.com>
2023-08-20 00:00:52 -0400

    update order model

--- inventory/dashboard/admin.py
+++ inventory/dashboard/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Order
 from django.contrib.auth.models import Group
 
 admin.site.site_header = "Inventory Management System"
@@ -17,3 +17,4 @@ class ProductAdmin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Order)
--- inventory/dashboard/models.py
+++ inventory/dashboard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 CATEGORIES = (
     ("Stationary", "Stationary"),
@@ -13,5 +14,21 @@ class Product(models.Model):
     category = models.CharField(choices=CATEGORIES, max_length=20, null=True)
     quantity = models.PositiveSmallIntegerField(null=True)
 
+    class Meta:
+        verbose_name_plural = "Product"
+
     def __str__(self):
-        return f"{self.name} - {self.quantity}"
+        return self.name
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    staff = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    order_quantity = models.PositiveSmallIntegerField(null=True)
+    date = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "Order"
+
+    def __str__(self):
+        return f"{self.product} - ordered by {self.staff.username}"
