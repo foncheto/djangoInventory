01800cc03242d1666cc1e2f3c5dd8abe5dd2ee20
Alfonso <81327113+foncheto@users.noreply.github.com>
2023-08-20 13:45:49 -0400

    user app and registration

--- inventory/inventory/settings.py
+++ inventory/inventory/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "dashboard.apps.DashboardConfig",
+    "user.apps.UserConfig",
+    "crispy_forms",
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -100,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
---
+++ inventory/user/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+
+
+# Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            # Save the user to the database
+            form.save()
+            return redirect("dashboard-index")
+    else:
+        form = CreateUserForm()
+    context = {"form": form}
+    return render(request, "user/register.html", context)
