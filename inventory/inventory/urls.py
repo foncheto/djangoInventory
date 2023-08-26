6a6f14fd01bc913f98099dc54a8a9fc980c493a2
Alfonso <81327113+foncheto@users.noreply.github.com>
2023-08-26 14:48:54 -0400

    update loginlogout page

--- inventory/inventory/urls.py
+++ inventory/inventory/urls.py
@@ -17,9 +17,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from user import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("dashboard.urls")),
-    path("register/", user_views.register, name="register"),
+    path("register/", user_views.register, name="user-register"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="user/login.html"),
+        name="user-login",
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(template_name="user/logout.html"),
+        name="user-logout",
+    ),
 ]
